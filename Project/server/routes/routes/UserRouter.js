984279410b6a7fd61282baab28ad1c8137f4ec4c
Sanjay Nair <49430599+nairsanjay@users.noreply.github.com>
2021-04-07 16:55:13 +0530

    Deleting older file to follow Sir's guideline on how to use git.

--- Project/server/routes/routes/UserRouter.js
+++
@@ -1,6 +0,0 @@
-var router = require('express').Router()
-const UserService = require('../services/UserService')
-
-router.route("/login").get(UserService.loginGet).post(UserService.login)
-
-module.exports = router;
